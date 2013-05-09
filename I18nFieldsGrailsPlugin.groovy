e26c368fd0c242ade1a17153c4a67526c72f440a
Jorge Uriarte <jorge.uriarte@omelas.net>
2013-05-09 12:47:53 +0200

    Updated to 0.6.4

--- I18nFieldsGrailsPlugin.groovy
+++ I18nFieldsGrailsPlugin.groovy
@@ -9,7 +9,7 @@ import org.hibernate.event.PostInsertEvent
 class I18nFieldsGrailsPlugin {
     static final def log = Logger.getLogger(I18nFieldsGrailsPlugin)
 
-	def version = "0.6.3"
+	def version = "0.6.4"
 	def grailsVersion = "1.3 > *"
 	def dependsOn = ['yammerMetrics':'0.2-SNAPSHOT']
 	def pluginExcludes = [
