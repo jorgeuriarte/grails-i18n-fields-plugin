7863bed45fbe1751a7b348b991719a60bb89d9b6
Jorge Uriarte <jorge.uriarte@omelas.net>
2013-05-09 11:53:35 +0200

    Grails version upgraded to 2.2.1

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,4 +1,5 @@
 i18nFields {
 	locales = ["es", "es_MX", "en_US", "kl_KL", "eu"]
 	extraLocales = ["eu"]
-}
+}grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
