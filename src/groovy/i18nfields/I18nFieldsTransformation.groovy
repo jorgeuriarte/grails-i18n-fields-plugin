9e50994a1ef9c173ff9346abf4146cdea53ec0c7
Fernando Niwes <fernando@soluciones3f.com.ar>
2013-07-04 18:18:13 -0300

    Removed support for deprecated configuration parameters

--- src/groovy/i18nfields/I18nFieldsTransformation.groovy
+++ src/groovy/i18nfields/I18nFieldsTransformation.groovy
@@ -20,21 +20,12 @@ public class I18nFieldsTransformation implements ASTTransformation {
 	void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
 		if (!isValidAstNodes(astNodes))
 			return
-		checkForDeprecatedConfiguration()
 		ClassI18nalizator internationalizator = new ClassI18nalizator(astNodes[1], locales(), redisLocales())
 		internationalizator.transformClass()
 	}
 
 	private isValidAstNodes(ASTNode[] astNodes) {
 		return astNodes != null && astNodes[0] != null && astNodes[1] != null && astNodes[0] instanceof AnnotationNode && astNodes[0].classNode?.name == I18nFields.class.getName() && astNodes[1] instanceof ClassNode
-	}
-
-	// TODO: Use log4j
-	private checkForDeprecatedConfiguration() {
-		if (pluginConfig."${I18nFields.I18N_FIELDS}"?.containsKey(I18nFields.DEPRECATED_LOCALES))
-			println "[i18nFields] WARNING - Configuration is using a deprecated locale list definition (i18n_langs). Change it for 'locales' as soon as possible"
-		if (pluginConfig."${I18nFields.I18N_FIELDS}"?.containsKey(I18nFields.DEPRECATED_EXTRA_LOCALES))
-			println "[i18nFields] WARNING - Configuration is using a deprecated extra locale list definition (i18n_langs). Change it for 'locales' as soon as possible"
 	}
 
 	private locales() {
@@ -52,8 +43,6 @@ public class I18nFieldsTransformation implements ASTTransformation {
 	private getConfiguredLocales() {
 		if (null != pluginConfig."${I18nFields.I18N_FIELDS}"?."${I18nFields.LOCALES}")
 			return pluginConfig."${I18nFields.I18N_FIELDS}"."${I18nFields.LOCALES}"
-		if (null != pluginConfig."${I18nFields.I18N_FIELDS}"?."${I18nFields.DEPRECATED_LOCALES}")
-			return pluginConfig."${I18nFields.I18N_FIELDS}"."${I18nFields.DEPRECATED_LOCALES}"
 		return [:]
 	}
 	
@@ -83,8 +72,6 @@ public class I18nFieldsTransformation implements ASTTransformation {
 	private getExtraLocales() {
 		if (null != pluginConfig."${I18nFields.I18N_FIELDS}"?."${I18nFields.EXTRA_LOCALES}")
 			return pluginConfig."${I18nFields.I18N_FIELDS}"."${I18nFields.EXTRA_LOCALES}"
-		if (null != pluginConfig."${I18nFields.I18N_FIELDS}"?."${I18nFields.DEPRECATED_EXTRA_LOCALES}")
-			return pluginConfig."${I18nFields.I18N_FIELDS}"."${I18nFields.DEPRECATED_EXTRA_LOCALES}"
 		return [:]
 	}
 
