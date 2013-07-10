3c68a8b2560e5599b24db578cbc3ad4963c95d9f
Fernando Niwes <fernando@soluciones3f.com.ar>
2013-07-10 14:29:48 -0300

    Removed useless configuration from plugin

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,6 +1,2 @@
-i18nFields {
-	locales = ["es", "es_MX", "en_US", "kl_KL", "eu"]
-	extraLocales = ["eu"]
-}
 grails.views.default.codec="none" // none, html, base64
 grails.views.gsp.encoding="UTF-8"
