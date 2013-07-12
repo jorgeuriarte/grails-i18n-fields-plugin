951f4ed852534645a8f38decc0fc535d48bc11fe
Fernando Niwes <fernando@soluciones3f.com.ar>
2013-07-12 16:52:12 -0300

    Back again to old way of persist to redis. New event listeners do not have domain property set.

--- I18nFieldsGrailsPlugin.groovy
+++ I18nFieldsGrailsPlugin.groovy
@@ -57,10 +57,22 @@ class I18nFieldsGrailsPlugin {
     }
 
     def doWithApplicationContext = { applicationContext ->
-    	application.mainContext.eventTriggeringInterceptor.datastores.each { k, datastore ->
-            def listener = new I18nFieldsPersistenceEventListener(datastore)
-            applicationContext.addApplicationListener(listener)
+        def listeners = applicationContext.sessionFactory.eventListeners
+        def listener = new I18nFieldsListener()
+        ['saveOrUpdate', 'preDelete'].each {
+            addEventTypeListener(listeners, listener, it)
         }
+    }
+
+    private void addEventTypeListener(listeners, listener, type) {
+        def typeProperty = "${type}EventListeners"
+        def typeListeners = listeners."${typeProperty}"
+
+        def expandedTypeListeners = new Object[typeListeners.length + 1]
+        System.arraycopy(typeListeners, 0, expandedTypeListeners, 0, typeListeners.length)
+        expandedTypeListeners[-1] = listener
+
+        listeners."${typeProperty}" = expandedTypeListeners
     }
 
 }
---
+++ src/groovy/i18nfields/I18nFieldsListener.groovy
@@ -0,0 +1,49 @@
+package i18nfields;
+
+import org.hibernate.event.Initializable;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.event.PreInsertEvent;
+import org.hibernate.event.PreInsertEventListener;
+import org.hibernate.event.PreDeleteEvent;
+import org.hibernate.event.PreDeleteEventListener;
+import org.hibernate.event.PostDeleteEventListener;
+import org.hibernate.event.PostDeleteEvent;
+import org.hibernate.event.PostInsertEvent;
+import org.hibernate.event.PostInsertEventListener;
+import org.hibernate.event.PreUpdateEvent;
+import org.hibernate.event.PreUpdateEventListener;
+import org.hibernate.event.PostUpdateEvent;
+import org.hibernate.event.PostUpdateEventListener;
+import org.hibernate.event.SaveOrUpdateEvent;
+import org.hibernate.event.SaveOrUpdateEventListener;
+import org.hibernate.event.Initializable;
+
+public class I18nFieldsListener implements SaveOrUpdateEventListener, PreDeleteEventListener {
+
+	public void onSaveOrUpdate(final SaveOrUpdateEvent event) {
+	    saveEntity(event.entity);
+	}
+
+	public boolean onPreDelete(final PreDeleteEvent event) {
+    	deleteEntity(event.entity);
+	}
+
+    /**
+     * Persist entity values.
+     * At this point entity should have a id
+     */
+    protected void saveEntity(entity) {
+        if(!entity.hasProperty(I18nFields.I18N_FIELDS)) return; // Entity should have i18n
+        // TODO: test if a i18n field is dirty
+        I18nFieldsHelper.pushAll(entity)
+    }
+    
+    /**
+     * Delete a entity.
+     * At this point the entity should ahve a id
+     */
+    protected void deleteEntity(entity) {
+        if(!entity.hasProperty(I18nFields.I18N_FIELDS)) return; // Entity should have i18n
+        I18nFieldsHelper.delete(entity)
+    }
+}
