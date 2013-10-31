9054e80561a375d9a0883fd9950dd3897968a3cd
Fernando Niwes <fniwes@gmail.com>
2013-10-31 11:23:15 -0300

    Increased version number

--- I18nFieldsGrailsPlugin.groovy
+++ I18nFieldsGrailsPlugin.groovy
@@ -9,7 +9,7 @@ import org.hibernate.event.PostInsertEvent
 class I18nFieldsGrailsPlugin {
     static final def log = Logger.getLogger(I18nFieldsGrailsPlugin)
 
-	def version = "0.7.8-SNAPSHOT"
+	def version = "0.7.11-SNAPSHOT"
 	def grailsVersion = "2.1.0 > *"
 	def pluginExcludes = [
 			"lib/*",
