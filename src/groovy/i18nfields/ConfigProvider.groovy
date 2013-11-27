b7132c271c67c46b0506151069554d5372eb10fc
Jorge Uriarte <jorge.uriarte@omelas.net>
2013-11-27 18:16:22 +0100

    Avoid re-parsing of config file explicitely, (it took years...)

--- src/groovy/i18nfields/ConfigProvider.groovy
+++ src/groovy/i18nfields/ConfigProvider.groovy
@@ -5,8 +5,11 @@ import grails.util.BuildSettingsHolder
 
 class ConfigProvider {
 	static final CONFIG_LOCATION = "${BuildSettingsHolder.settings.baseDir}/grails-app/conf/Config.groovy"
-
+	static def config
 	def getConfig() {
-		return new ConfigSlurper(GrailsUtil.environment).parse(new File(CONFIG_LOCATION).toURL())
+		if (!config) {
+			config = new ConfigSlurper(GrailsUtil.environment).parse(new File(CONFIG_LOCATION).toURL())
+		}
+		return config
 	}
 }
